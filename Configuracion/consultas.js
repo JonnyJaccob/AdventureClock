f1a7b863f1f7214a6f077a6c38e64de0cbfe04b3
Josue <jaccobjosue@gmail.com>
2023-12-04 17:30:19 -0600

    t

--- Configuracion/consultas.js
+++ Configuracion/consultas.js
@@ -8,7 +8,7 @@ const dataDeBase = {
     user: process.env.USER || 'root',
     password: process.env.PASSWORD || '',
     database: process.env.DATABASE || 'adventure',
-    connectTimeout: 120000
+    connectTimeout: 10000
 }
 
 /**
